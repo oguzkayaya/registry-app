9bf1ea846456f16a1b8efb816755300c9eaaff99
oguz <oguzkayaya@gmail.com>
2020-12-20 11:50:22 +0300

    create table if not exist in db

--- src/main/java/org/ok/service/EntryService.java
+++ src/main/java/org/ok/service/EntryService.java
@@ -11,16 +11,9 @@ public class EntryService {
 
     private EntryRepository repository;
 
-    public EntryRepository getRepository() {
-        return repository;
-    }
-
-    public void setRepository(EntryRepository repository) {
+    public EntryService(EntryRepository repository) {
         this.repository = repository;
     }
-/* public EntryService(EntryRepository repository) {
-        this.repository = repository;
-    } */
 
     public void saveEntry(Entry entry) {
         repository.save(entry);
